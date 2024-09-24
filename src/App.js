345e755732e6afe18f278549f8e305e21df31c7e
ArturoSanchez <solounavez00@gmail.com>
2024-09-24 09:41:22 -0600

    funciones vs componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import MiComponente from "./components/MiComponente";
 
-function App() {
+// Las funciones pueden recibir valores normales
+function multiplicacion(a,b) {
+  // Pueden retornar varias diferentes 
+  return a*b;
+}
 
+function App() {
+  
   const nombre = "Bill Gates";
 
+  // Retorna JSX
   return (
     <div >
       <MiComponente nombre={nombre} />
+      <p>{multiplicacion(15,10)}</p>
     </div>
   );
 }
