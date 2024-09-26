1ee4a22ef82fa6b2aa84f69cb330177e14cb68d2
ArturoSanchez <solounavez00@gmail.com>
2024-09-26 08:48:57 -0600

    Elimino ejemplo de html, css y js

--- react-vs-html-css-js/script.js
+++
@@ -1,7 +0,0 @@
-function actualizarSaludo() {
-  const nombre = document.getElementById('nombre').value.trim();
-  const saludo = document.getElementById('saludo');
-  
-  // Actualiza el texto del saludo con el nombre ingresado
-  saludo.textContent = 'Hola ' + (nombre ? nombre : '');
-}
